f8bd18ed3d134718f33c29df5c3e9bae7c055ee1
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-02-06 22:49:38 +0600

    made the about page responsive

---
+++ src/Components/InstagramPost/InstagramPost.jsx
@@ -0,0 +1,53 @@
+
+
+const img19 = "https://i.ibb.co/ygVGcbY/ig-9.jpg";
+const img2 = "https://i.ibb.co/DpkZVRd/coffee-1.jpg";
+const img3 = "https://i.ibb.co/6JmNTGC/coffee-2.jpg";
+const img4 = "https://i.ibb.co/vHjbJKf/ig-20.jpg";
+const img5 = "https://i.ibb.co/CW9zP8T/blog-5-140x140-jpg.webp";
+const img6 = "https://i.ibb.co/PrKSzcT/coffee-bg.jpg ";
+const img7 = "https://i.ibb.co/MSQTgnX/food-3.jpg";
+const img8 = "https://i.ibb.co/D4ZXrpN/st-coffee8.jpg";
+const img9 = "https://i.ibb.co/v3xhWys/restaurant.jpg";
+const img10 = "https://i.ibb.co/YLBvjBt/ig-7-255x255.jpg";
+const img11 = "https://i.ibb.co/QYm3VD8/coffee-4.jpg";
+const img12 = "https://i.ibb.co/MnfLLcc/ig-16.jpg";
+const img13 = "https://i.ibb.co/QfWCSx9/ig-19-370x370.jpg";
+const img14 = "https://i.ibb.co/yyS7ykV/ig-4-300x300.jpg";
+const img15 = "https://i.ibb.co/kGB5dvN/coffee-bg-1.jpg";
+
+
+
+const InstagramPost = () => {
+
+    return (
+
+        <div className="w-full mx-auto relative mt-14">
+            <div className="grid grid-cols-3 md:grid-cols-6 gap-2">
+                <img src={img19} alt="" className="md:col-span-2 md:row-span-2 justify-self-stretch" />
+                <img src={img2} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img3} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img4} alt="" className="md:col-span-2 md:row-span-2 self-stretch justify-self-stretch" />
+                <img src={img5} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img6} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img7} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img8} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img9} alt="" className="md:col-span-2 md:row-span-2 self-stretch justify-self-stretch" />
+                <img src={img10} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img11} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img12} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img13} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img14} alt="" className="self-stretch justify-self-stretch" />
+                <img src={img15} alt="" className="self-stretch justify-self-stretch" />
+            </div>
+
+            <div className="bg-white w-[250px] lg:w-[300px] h-[250px] lg:h-[300px] rounded-[50%] flex justify-center items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[99] text-second hover:text-main duration-300">
+                <p className="text-center font-body font-medium capitalize text-2xl">Follow Us on <br />Instagram</p>
+            </div>
+        </div>
+
+    )
+        ;
+};
+
+export default InstagramPost;
--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import ButtonMain from "../../Components/ButtonMain/ButtonMain";
-import CenteredSectionTitle from "../../Components/CenteredSectionTitle/CenteredSectionTitle";
 import SectionTitle from "../../Components/SectionTitle/SectionTitle";
+import InstagramPost from "../../Components/InstagramPost/InstagramPost";
 
 
 // image url
@@ -10,29 +10,7 @@ const img1 = "https://i.ibb.co/f4Y0n9W/brooke-cagle-9f-HMo1-5-Io8-unsplash-2.jpg
 const iImg2 = "https://i.ibb.co/nMjL60G/mike-marquez-384421-unsplash.jpg";
 
 
-const img19 = "https://i.ibb.co/ygVGcbY/ig-9.jpg";
-const img2 = "https://i.ibb.co/DpkZVRd/coffee-1.jpg";
-const img3 = "https://i.ibb.co/6JmNTGC/coffee-2.jpg";
-const img4 = "https://i.ibb.co/vHjbJKf/ig-20.jpg";
-const img5 = "https://i.ibb.co/CW9zP8T/blog-5-140x140-jpg.webp";
-const img6 = "https://i.ibb.co/PrKSzcT/coffee-bg.jpg ";
-const img7 = "https://i.ibb.co/MSQTgnX/food-3.jpg";
-const img8 = "https://i.ibb.co/D4ZXrpN/st-coffee8.jpg";
-const img9 = "https://i.ibb.co/v3xhWys/restaurant.jpg";
-const img10 = "https://i.ibb.co/YLBvjBt/ig-7-255x255.jpg";
-const img11 = "https://i.ibb.co/QYm3VD8/coffee-4.jpg";
-const img12 = "https://i.ibb.co/MnfLLcc/ig-16.jpg";
-const img13 = "https://i.ibb.co/QfWCSx9/ig-19-370x370.jpg";
-const img14 = "https://i.ibb.co/yyS7ykV/ig-4-300x300.jpg";
-const img15 = "https://i.ibb.co/kGB5dvN/coffee-bg-1.jpg";
-
-
-
-
-
 const AboutUs = () => {
-
-
 
 
     return (
@@ -51,12 +29,12 @@ const AboutUs = () => {
 
 
             {/* about section */}
-            <div className="mt-14 container mx-auto flex justify-start items-center gap-10 px-10 py-7">
-                <div className="w-1/2">
+            <div className="mt-14 container mx-auto flex flex-col md:flex-row justify-start items-center gap-10 px-5 lg:px-10 py-7">
+                <div className="w-full md:w-1/2">
                     <img src={img1} alt="" className="rounded-t-[250px]" />
                 </div>
 
-                <div className="w-1/2 flex flex-col justify-start items-start gap-7">
+                <div className="w-full md:w-1/2 flex flex-col justify-start items-start gap-7">
                     <SectionTitle smallText={"Welcome to Caffeina!"} bigText={"Our Story"} />
                     <p className="font-body text-lightWhite">
 
@@ -71,36 +49,14 @@ const AboutUs = () => {
                 </div>
             </div>
 
-            {/* instagram post */}
-            <div className="w-full mx-auto relative mt-14">
-                <div className="grid grid-cols-6 gap-2">
-                    <img src={img19} alt="" className="col-span-2 row-span-2 justify-self-stretch" />
-                    <img src={img2} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img3} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img4} alt="" className="col-span-2 row-span-2 self-stretch justify-self-stretch" />
-                    <img src={img5} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img6} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img7} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img8} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img9} alt="" className="col-span-2 row-span-2 self-stretch justify-self-stretch" />
-                    <img src={img10} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img11} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img12} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img13} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img14} alt="" className="self-stretch justify-self-stretch" />
-                    <img src={img15} alt="" className="self-stretch justify-self-stretch" />
-                </div>
-
-                <div className="bg-white w-[300px] h-[300px] rounded-[50%] flex justify-center items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-[99] text-second hover:text-main duration-300">
-                    <p className="text-center font-body font-medium capitalize text-2xl">Follow Us on <br />Instagram</p>
-                </div>
-            </div>
+            {/* instagram section */}
+            <InstagramPost />
 
 
             {/* philosophy section */}
-            <div className="mt-14 container mx-auto flex justify-start items-center gap-10 px-10 py-7">
+            <div className="mt-14 container mx-auto flex flex-col md:flex-row justify-start items-center gap-10 px-5 lg:px-10 py-7">
 
-                <div className="w-1/2 flex flex-col justify-start items-start gap-7">
+                <div className="w-full md:w-1/2 flex flex-col justify-start items-start gap-7">
                     <SectionTitle smallText={"Great experience with us"} bigText={"Our Philosophy"} />
                     <p className="font-body text-lightWhite">
                         Step into the warm embrace of Caffeina, where every corner is infused with a welcoming ambiance.
@@ -112,7 +68,7 @@ const AboutUs = () => {
                     <Link to={"/findATable"}><ButtonMain buttonText={"Reservation"} /></Link>
                 </div>
 
-                <div className="w-1/2">
+                <div className="w-full md:w-1/2">
                     <img src={iImg2} alt="" className="rounded-t-[250px]" />
                 </div>
             </div>
